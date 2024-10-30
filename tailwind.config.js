96f282b260b33ba06a02bfb11e46a9a47a285c84
Pratik Tandel <pratik.tandel@1rivet.com>
2024-10-30 21:50:27 +0530

    feat: update layout

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#026197'
+      },
+      backgroundColor: {
+        'primary': '#026197'
+      }
+    },
+  },
+  plugins: [],
+}
+
